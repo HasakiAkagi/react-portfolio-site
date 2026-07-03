6d654437037f9dd67f2df67bf39e7fc56f898309
HasakiAkagi <hasaki.akagi@nsw.co.jp>
2026-07-03 11:51:02 +0900

    fix skills duplicate declaration

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,39 +4,6 @@ import { requestStates } from '../constants';
 import { useSkills } from '../customHooks/useSkills';
 
 export const Skills = () => {
-  const [state, dispatch] = useReducer(skillReducer, initialState);
-
-  useEffect(() => {
-    dispatch({ type: actionTypes.fetch });
-    axios.get('https://api.github.com/users/USER_NAME/repos')
-      .then((response) => {
-        const languageList = response.data.map(res => res.language);
-        const countedLanguageList = generateLanguageCountObj(languageList);
-        dispatch({ type: actionTypes.success, payload: { languageList: countedLanguageList } });
-      })
-      .catch(() => {
-        dispatch({ type: actionTypes.error });
-      });
-  }, []);
-
-  const generateLanguageCountObj = (allLanguageList) => {
-    const notNullLanguageList = allLanguageList.filter(language => language != null);
-    const uniqueLanguageList = [...new Set(notNullLanguageList)];
-
-    return uniqueLanguageList.map(item => {
-      return {
-        language: item,
-        count: allLanguageList.filter(language => language === item).length
-      }
-    });
-  };
-  const convertCountToPercentage = (count) => {
-    if (count > 10) { return 100; }
-    return count * 10;
-  };
-  const sortedLanguageList = () => (
-    state.languageList.sort((firstLang, nextLang) => nextLang.count - firstLang.count)
-  )
   const [sortedLanguageList, fetchRequestState, converseCountToPercentage] = useSkills();
   return (
     <div id="skills">
